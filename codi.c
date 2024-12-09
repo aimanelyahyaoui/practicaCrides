ac38b855bddf1ea59cf6eba97c2de74488dd7e4f
rogercampos006 <1750478@uab.cat>
2024-12-09 10:15:32 +0100

    Update codi.c

--- codi.c
+++ codi.c
@@ -1,3 +1,15 @@
 #include <fcntl.h>
 #include <uistd.h>
 
+int main(int argc, char *argv[]){
+
+        if(argc<2); {
+                perror("Error, falten arguments");
+                return -1;
+                }
+
+        int fd = open (argv[1], O_CREAT | O_WRONLY | O_TRUNC, 0644);
+        if (fd == -1){
+                perror("No s'ha pogut crear el fitxer");
+                }
+        }
