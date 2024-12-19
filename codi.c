5d98ca724ea20ff55e07e767848d56717962bfcd
aimanelyahyaoui <1749573@uab.cat>
2024-12-19 16:24:46 +0100

    Update codi.c

--- codi.c
+++ codi.c
@@ -1,42 +1,44 @@
 #include <fcntl.h>
 #include <unistd.h>
+#include <stdio.h>
 
-int main(int argc, char *argv[]){
+int main(int argc, char *argv[]) {
 
-        if(argc<2) {
-                perror("Error, falten arguments");
-                return -1;
-                }
+    if (argc < 2) {
+        perror("Error, falten arguments");
+        return -1;
+    }
 
-        int fd = open (argv[1], O_CREAT | O_WRONLY | O_TRUNC, 0644);
-        if (fd == -1){
-                perror("No s'ha pogut crear el fitxer");
-                return -1;
-        }
+    int fd = open(argv[1], O_CREAT | O_WRONLY | O_TRUNC, 0644);
+    if (fd == -1) {
+        perror("No s'ha pogut crear el fitxer");
+        return -1;
+    }
 
     char caracter;
     ssize_t bytesLlegits;
 
-    while ((bytesLlegits = read(STDIN_FILENO, &caracter, 1)) != 0 && -1) {
-        if ((write(STDOUT_FILENO, &caracter, 1) == -1) || write(fitxer, &caracter, 1) == -1) {
+    while ((bytesLlegits = read(STDIN_FILENO, &caracter, 1)) != 0) {
+        if (bytesLlegits == -1 || write(STDOUT_FILENO, &caracter, 1) == -1 || write(fd, &caracter, 1) == -1) {
             perror("Error durant l'escriptura");
-            close(fitxer);
+            close(fd);
             return -1;
         }
     }
 
     if (bytesLlegits == -1) {
         perror("Error durant la lectura");
-        close(fitxer);
+        close(fd);
         return -1;
     }
 
-    if (close(fitxer) == -1) {
+    if (close(fd) == -1) {
         perror("Error en tancar el fitxer");
         return -1;
     }
 
     return 0;
+}
 
 #Segon programa:
 
