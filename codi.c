3c352b2e7a3443119fe9a133bdea641153dac93f
rogercampos006 <1750478@uab.cat>
2024-12-19 10:57:36 +0100

    Update codi.c

--- codi.c
+++ codi.c
@@ -37,3 +37,77 @@ int main(int argc, char *argv[]){
     }
 
     return 0;
+
+#Segon programa:
+
+
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define BUFFER_MIDA 1024
+
+int main(int argc, char *argv[]) {
+    int fd, fd2;
+
+    if (argc < 3) {
+        perror("No hi ha prous arxius");
+        return -1;
+    }
+
+    fd = open(argv[1], O_RDONLY);
+    if (fd == -1) {
+        perror("Error intentant obrir el primer arxiu");
+        return -1;
+    }
+
+    fd2 = open(argv[2], O_RDONLY);
+    if (fd2 == -1) {
+        perror("Error intentant obrir el segon arxiu");
+        close(fd);
+        return -1;
+    }
+
+    int comptador_bytes = 1;
+    int comptador_linies = 1;
+    ssize_t bytes_llegits1, bytes_llegits2;
+    char buf1[BUFFER_MIDA], buf2[BUFFER_MIDA];
+
+    while (1) {
+        bytes_llegits1 = read(fd, buf1, 1);
+        bytes_llegits2 = read(fd2, buf2, 1);
+
+        if (bytes_llegits1 == -1 || bytes_llegits2 == -1) {
+            perror("Error llegint els fitxers");
+            close(fd);
+            close(fd2);
+            return -1;
+        }
+
+        if (bytes_llegits1 == 0 && bytes_llegits2 == 0) {
+            printf("Els arxius són iguals\n");
+            close(fd);
+            close(fd2);
+            return 0;
+        }
+
+        if (bytes_llegits1 != bytes_llegits2 || buf1[0] != buf2[0]) {
+            printf("Els arxius es diferèncien al byte %d de la línia %d\n",
+                   comptador_bytes, comptador_linies);
+            close(fd);
+            close(fd2);
+            return 0;
+        }
+
+        if (buf1[0] == '\n') {
+            comptador_linies++;
+        }
+
+        comptador_bytes++;
+    }
+
+    close(fd);
+    close(fd2);
+    return 0;
+}
